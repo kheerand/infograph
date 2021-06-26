0aef6f8bbc721630cc7fd5355e492c228141ef6f
Kheeran D <kheerand@gmail.com>
2021-06-27 00:12:13 +1000

    Added loading input type via config file

--- src/components/infoTypeInfoSnippet.vue
+++ src/components/infoTypeInfoSnippet.vue
@@ -14,7 +14,12 @@
           {{ subject.label }}
         </td>
         <td class="input_input">
-          <input type="text" v-model="subject.value" />
+          <span v-if="subject.type == 'textarea'">
+            <textarea rows=4 cols=50 v-model="subject.value" />
+          </span>
+          <span v-else>
+            <input type="subject.type" v-model="subject.value" />
+          </span>
         </td>
       </tr>
     </table>
@@ -43,10 +48,10 @@
         <span v-for="subject in fields.subjects" :key="subject">
           <span v-if="subject.value != ''">
             <span> :{{ fields.predicate.value }} {{ subject.predicate }} </span>
-            <span v-if="subject.type == 'l'">
+            <span v-if="subject.literal == true">
               "{{ subject.value }}".<br />
             </span>
-            <span v-if="subject.type == 's'">
+            <span v-else>
               :{{ subject.value }}.<br />
             </span>
           </span>
@@ -64,6 +69,7 @@ export default {
   props: {},
   data() {
     return {
+      json: null,
       prefixes: json.prefixes,
       fields: json.fields
     }
