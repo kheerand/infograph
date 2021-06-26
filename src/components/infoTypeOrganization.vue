280db5c40ccbc03d5cfe295610c42b211e3233e0
Kheeran D <kheerand@gmail.com>
2021-06-27 00:33:53 +1000

    Refactored to render using config file.

--- src/components/infoTypeOrganization.vue
+++ src/components/infoTypeOrganization.vue
@@ -1,118 +1,79 @@
 <template>
-  <div id='input_area'>
-    <h3>Organisation</h3>
-    <p>
-      <table id='input_table'>
-        <tr class='input_row'>
-          <td class='input_heading'>
-            Organisation ID
-          </td>
-          <td class='input_input'>
-            <input type="text" v-model="nameID" />
-          </td>
-        </tr>
-        <tr class='input_row'>
-          <td class='input_heading'>
-            Organisation name
-          </td>
-          <td class='input_input'>
-            <input type="text" v-model="name" />
-          </td>
-        </tr>
-        <tr class='input_row'>
-          <td class='input_heading'>
-            Description
-          </td>
-          <td class='input_input'>
-            <textarea rows=4 cols=50 v-model="description" />
-          </td>
-        </tr>
-        <tr class='input_row'>
-          <td class='input_heading'>
-            Funder ID
-          </td>
-          <td class='input_input'>
-            <input type="text" v-model="funderID" />
-          </td>
-        </tr>
-        <tr class='input_row'>
-          <td class='input_heading'>
-            Parent organisation ID
-          </td>
-          <td class='input_input'>
-            <input type="text" v-model="parentOrganizationID" />
-          </td>
-        </tr>
-        <tr class='input_row'>
-          <td class='input_heading'>
-            URL
-          </td>
-          <td class='input_input'>
-            <input type="text" v-model="url" />
-          </td>
-        </tr>
-      </table>
-    </p>
-  </div>
-  <div id="turtle_snippet">
-    <div id='turtle_preamble'>
-      <h4>Turtle snippet</h4>
-      <p>
-        <em>Copy and paste the turtle snippet below to GraphDB</em>
-      </p>
-      <hr>
-    </div>
-    <div id="turtle_prefixes">
-      <p>
-        <span v-for="prefix in prefixes" :key="prefix">
-            {{prefix}}<br>
-        </span>
-      </p>
-    </div>
+  <div id="input_area">
+    <h3>{{title}}</h3>
+    <table>
+      <tr class="input_row">
+        <td class="input_heading">
+          {{ fields.predicate.label }}
+        </td>
+        <td class="input_input">
+          <input type="text" v-model="fields.predicate.value" />
+        </td>
+      </tr>
+      <tr class="input_row" v-for="subject in fields.subjects" :key="subject">
+        <td class="input_heading">
+          {{ subject.label }}
+        </td>
+        <td class="input_input">
+          <span v-if="subject.type == 'textarea'">
+            <textarea rows=4 cols=50 v-model="subject.value" />
+          </span>
+          <span v-else>
+            <input :type="subject.type" v-model="subject.value" />
+          </span>
+        </td>
+      </tr>
+    </table>
 
-    
-    <div id="turtle_tripples">
-      <span v-if="nameID != ''">
-        :{{nameID}} a schema:Organzation.<br>
-      </span>
-      <span v-if="name != ''">
-        :{{nameID}} schema:name "{{name}}".<br>
-        :{{nameID}} rdfs:label "{{name}}".<br>
-      </span>
-      <span v-if="description != ''">
-        :{{nameID}} schema:description "{{description}}".<br>
-      </span>
-      <span v-if="funderID != ''">
-        :{{nameID}} schema:funder :{{funderID}}.<br>
-      </span>
-      <span v-if="parentOrganizationID != ''">
-        :{{nameID}} schema:parentOrganization :{{parentOrganizationID}}.<br>
-      </span>
-      <span v-if="url != ''">
-        :{{nameID}} schema:url "{{url}}".<br>
-      </span>
+    <div id="turtle_snippet">
+      <div id="turtle_preamble">
+        <h4>Turtle snippet</h4>
+        <p>
+          <em>Copy and paste the turtle snippet below to GraphDB</em>
+        </p>
+        <hr />
+      </div>
+
+      <div id="turtle_prefixes">
+        <p>
+          <span v-for="prefix in prefixes" :key="prefix">
+            {{ prefix }}<br />
+          </span>
+        </p>
+      </div>
+
+      <div id="turtle_tripples">
+        <span v-if="fields.predicate.value != ''">
+          :{{ fields.predicate.value }} a {{ fields.predicate.class }}.<br />
+        </span>
+        <span v-for="subject in fields.subjects" :key="subject">
+          <span v-if="subject.value != ''">
+            <span> :{{ fields.predicate.value }} {{ subject.predicate }} </span>
+            <span v-if="subject.literal == true">
+              "{{ subject.value }}".<br />
+            </span>
+            <span v-else>
+              :{{ subject.value }}.<br />
+            </span>
+          </span>
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import json from '@/config/infoTypeOrganization.json'; // Change it so that this can be specified during app call.
+
 export default {
-  name: "OrganizationItem",
+  name: "turtleItem",
   props: {},
   data() {
     return {
-      prefixes: [
-        "@PREFIX : <http://my_info_graph/>", // try and load this from a config file in the future.
-        "@PREFIX schema: <https://schema.org>",
-        "@PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
-      ],
-      nameID: "",
-      name: "",
-      description: "",
-      funderID: "",
-      parentOrganizationID: "",
-      url: "",
-    };
+      title: json.title,
+      prefixes: json.prefixes,
+      fields: json.fields
+    }
   },
 };
 </script>
@@ -138,7 +99,7 @@ a {
   background: aliceblue;
 }
 #turtle_preamble {
-  background:beige;
+  background: beige;
 }
 .input_heading {
   text-align: right;
