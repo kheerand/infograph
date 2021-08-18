b5ef928679a5952f1a13235f525b0bfc68ecdc04
Kheeran D <kheerand@gmail.com>
2021-08-18 22:41:13 +1000

    Added ability to copy resource to clipboard and palyed with config.

--- src/components/renderResource.vue
+++ src/components/renderResource.vue
@@ -1,22 +1,20 @@
 <template>
-  <div v-if="triple.type == 'uri'">
-    <span style="cursor:pointer;" v-on:click="newSearch">
+  <div v-if="triple.type == 'uri'" >
+    <span style="cursor: pointer" v-on:click="newSearch">
       {{ triple.prefixedValue }}
-    </span><br>
-    <a class="graph_link" :href="triple.value" target="_blank">
-      [g]
-    </a>
-    <a class="graph_link" :href="triple.visualLink" target="_blank">
-      [v]
-    </a>
+    </span>
+    <br />
+    <a class="graph_link" :href="triple.value" target="_blank"> [g] </a>
+    <a class="graph_link" :href="triple.visualLink" target="_blank"> [v] </a>
+    <span class="graph_link" style="cursor: pointer" v-on:click="copyText">[c]</span>
   </div>
   <div v-else class="truncated_text">
     {{ triple.prefixedValue }}
   </div>
+  <input type="hidden" :id="copyID" :value="copyID">
 </template>
 
 <script>
-
 export default {
   name: "renderResource",
   props: {
@@ -27,10 +25,15 @@ export default {
   },
   mounted() {},
   beforeUpdate() {},
-  computed: {},
+  computed: {
+    copyID() {
+      let copyID = this.triple.prefixedValue.split(":")[1]
+      return(copyID)
+    }
+  },
   methods: {
     newSearch() {
-      console.log("Running newsearch with ", this.triple.prefixedValue)
+      console.log("Running newsearch with ", this.triple.prefixedValue);
 
       // We set the new query string and emit an event to trigger things to rerun.
       var queryString = `
@@ -43,13 +46,28 @@ export default {
           {?Subject ?Predicate ?LTR}
           UNION
           {?RTL ?Predicate ?Subject}.
-        } limit 50`
-      
-      queryString = queryString.replace(/\?Subject/g,this.triple.originalValue)
+        } limit 50`;
 
-      console.log('Query string: \n',queryString)
-      this.$emit('queryString',queryString)
-    }
+      queryString = queryString.replace(
+        /\?Subject/g,
+        this.triple.originalValue
+      );
+
+      console.log("Query string: \n", queryString);
+      this.$emit("queryString", queryString);
+    },
+    copyText() {
+      console.log("Copy text: ");
+      // var copyText = this.triple.prefixedValue.split(":")[1];
+      // navigator.clipboard.writeText(copyText)
+
+      let copyText = document.querySelector("#"+this.copyID)
+      copyText.setAttribute("type","text")
+      copyText.select()
+      document.execCommand("copy")
+      copyText.setAttribute("type","hidden")
+      console.log("Copy text: ", copyText);
+    },
   },
 };
 </script>
